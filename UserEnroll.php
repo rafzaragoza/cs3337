bb33ed5a3fc70ce86686c29a48d7cae017ce4415
Rafa <raf.zaragoza@gmail.com>
2019-03-01 19:17:46 -0800

    Add files via upload

---
+++ UserEnroll.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>User Enroll Page </title>
+</head>
+<body>
+<p align="center"> User Enroll Page </p>
+<form action="UserInsert.php" method="post">
+<table align="center">
+<tr>
+<td>
+User name
+</td>
+<td>
+<input type="text" name="name" />
+</td>
+</tr>
+<tr>
+<td>
+Email
+</td>
+<td>
+<input type="text" name="email" />
+</td>
+</tr>
+<tr>
+<td>
+
+</td>
+<td>
+<input type="submit" value="Submit"/>
+</td>
+</tr>
+</body>
+</html>
---
+++ userInsert.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Untitled Document </title>
+</head>
+
+<body>
+<?php
+	include("Connect_Database.php");
+?>
+	
+<?php
+$userInsert = "insert into users values('" .
+				$_POST["name"] .
+				"', '" .
+				$_POST["email"] .
+				"')";
+$result = mysqli_query($connect, $userInsert);
+?>
+</body>
+</html>
