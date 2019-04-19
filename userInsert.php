b35cb0fc27260e351f5f9f7ce605bbcbbd2690e1
mgillig <mgillig@calstatela.edu>
2019-04-19 10:23:16 -0700

    Update userInsert.php

--- userInsert.php
+++ userInsert.php
@@ -2,21 +2,24 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title> Untitled Document </title>
+<title>Untitled Document</title>
 </head>
 
 <body>
+
 <?php
 	include("Connect_Database.php");
 ?>
-	
+
 <?php
-$userInsert = "insert into users values('" .
-				$_POST["name"] .
-				"', '" .
-				$_POST["email"] .
-				"')";
-$result = mysqli_query($connect, $userInsert);
+	
+	$userInsert = 	"insert into users values('" .
+		$_POST["name"] .
+		"', '" .
+		$_POST["email"] .
+		"')";
+		
+		$result = mysqli_query($connect, $userInsert);
 ?>
 </body>
 </html>
