19745137313974de6dd5e53384c0117b62e40f43
mgillig <mgillig@calstatela.edu>
2019-04-19 10:23:11 -0700

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,15 +1,13 @@
 <!doctype html>
 <html>
 <head>
-<link href="MainMenu.css" type="text/css" rel="stylesheet" />
-
-<meta charset="utf-8">
-<title>Main Page</title>
+	<link href="MainMenu.css" type="text/css" rel="stylesheet"/>
+	<meta charset="utf-8">
+	<title>main</title>
 </head>
-
 <body>
-<?php
-	include("MainMenu.php");
-?>
+	<?php
+		include("MainMenu.php");
+	?>
 </body>
 </html>
