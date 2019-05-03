2a85493d6cf2de91bb0781fabf41a4c24c4422fa
mgillig <mgillig@calstatela.edu>
2019-05-02 18:50:27 -0700

    Create forum.php

---
+++ forum.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sorting Page</title>
+</head>
+
+<body>
+<?php
+	include("MainMenu.php");
+?>
+
+<?php
+	include("Connect_Database.php");
+?>
+
+<?php
+	$selectForum = "select * from forum";
+	$results = mysqli_query($connect, $selectForum);
+?>
+	
+<h1 align="center">Forum</h1>
+
+<?php
+	while($row = mysqli_fetch_assoc($results))
+	{
+		print "<div style='border-bottom-style: solid;width: 300px;'>";
+		print "<h5>";
+		print ($row["name"]);
+		print "~";
+		print ($row["posttime"]);
+		print "</h5>";
+		print "<p>";
+		print ($row["message"]);
+		print "</p>";
+		print "</div>";
+	}
+?>
+<form action="forumInsert.php" enctype="multipart/form-data" method="post">
+	<table align="center">
+		<tr>
+			<td>
+				User Name
+			</td>
+			<td>
+				<input type="text" name="name" value="<?php print $_GET['name'];?>"/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Message
+			</td>
+			<td>
+				<input type="text" name="message" />
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" value="submit" />
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
