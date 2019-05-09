69fba7ad0e004105618c66e25e8fc4d41b40b421
mgillig <mgillig@calstatela.edu>
2019-05-08 22:28:46 -0700

    Update shopping.php

--- shopping.php
+++ shopping.php
@@ -8,7 +8,7 @@
 
 <body>
 <?php
-	include("ShoppingMenu.php");
+		include("MainMenu.php");
 ?>
 
 <?php
@@ -19,51 +19,55 @@
 	$selectBooks = "select * from books";
 	$results = mysqli_query($connect, $selectBooks);
 ?>
-	
-<table align="center" border="2" width="400">
-    <tr>
-        <th>
-            Title
-        </th>
-        <th>
-            Name
-        </th>
-        <th>
-            Post Time
-        </th>
-        <th>
-            Book Picture
-        </th>
-    </tr>
-	
-<?php
-	while($row = mysqli_fetch_assoc($results))
-	{
-		print "<tr>";
-		print "<td>";
-		print ($row["title"]);
-		print "</td>";
-		print "<td>";
-		print ($row["name"]);
-		print "</td>";
-		print "<td>";
-		print ($row["posttime"]);
-		print "</td>";
+<div class="div_css">
+	<?php
+		include("ShoppingMenu.php");
+	?>	
+	<table align="center" border="2" width="400">
+		<tr>
+			<th>
+				Title
+			</th>
+			<th>
+				Name
+			</th>
+			<th>
+				Post Time
+			</th>
+			<th>
+				Book Picture
+			</th>
+		</tr>
 		
-		print "<td>";
-		print "<img src='";
-		print $row["picpath"] . "' height=50 width=50>";
-		print "</td>";
-		
-		/*print "<td>";
-		print "<a href='UserDelete.php?";
-		print "email=" . $row["email"] . "'>";
-		print "DELETE";
-		print "</a";
-		print "</td>";*/
-		print "</tr>";
-	}
-?>
-</table>
+	<?php
+		while($row = mysqli_fetch_assoc($results))
+		{
+			print "<tr>";
+			print "<td>";
+			print ($row["title"]);
+			print "</td>";
+			print "<td>";
+			print ($row["name"]);
+			print "</td>";
+			print "<td>";
+			print ($row["posttime"]);
+			print "</td>";
+			
+			print "<td>";
+			print "<img src='";
+			print $row["picpath"] . "' height=50 width=50>";
+			print "</td>";
+			
+			/*print "<td>";
+			print "<a href='UserDelete.php?";
+			print "email=" . $row["email"] . "'>";
+			print "DELETE";
+			print "</a";
+			print "</td>";*/
+			print "</tr>";
+		}
+	?>
+	</table>
+</div>
 </body>
 </html>
