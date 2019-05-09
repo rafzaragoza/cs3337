4d983ea2ca08ea7ab4c07518f927845cbd50509f
mgillig <mgillig@calstatela.edu>
2019-05-09 15:37:55 -0700

    Update Wishlist.php

--- Wishlist.php
+++ Wishlist.php
@@ -18,46 +18,47 @@
 	$selectWishlist = "select * from wishlist";
 	$results = mysqli_query($connect, $selectWishlist);
 ?>
-	
-<h1 align="center">Forum</h1>
+<div class="div_css">	
+	<h1 class="div_css_h">Wishlist</h1>
 
-<?php
-	while($row = mysqli_fetch_assoc($results))
-	{
-		print "<div style='border-bottom-style: solid;width: 300px;'>";
-		print "<h5>";
-		print ($row["title"]);
-		print "</h5>";
-		print "<p>";
-		print ($row["description"]);
-		print "</p>";
-		print "</div>";
-	}
-?>
-<form action="WishlistInsert.php" enctype="multipart/form-data" method="post">
-	<table align="center">
-		<tr>
-			<td>
-				Title
-			</td>
-			<td>
-				<input type="text" name="title" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-				Description
-			</td>
-			<td>
-				<input type="text" name="description" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<input type="submit" value="submit" />
-			</td>
-		</tr>
-	</table>
-</form>
+	<?php
+		while($row = mysqli_fetch_assoc($results))
+		{
+			print "<div style='border-bottom-style: solid;width: 300px;'>";
+			print "<h5>";
+			print ($row["title"]);
+			print "</h5>";
+			print "<p>";
+			print ($row["description"]);
+			print "</p>";
+			print "</div>";
+		}
+	?>
+	<form action="WishlistInsert.php" enctype="multipart/form-data" method="post">
+		<table align="center">
+			<tr>
+				<td>
+					Title
+				</td>
+				<td>
+					<input type="text" name="title" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Description
+				</td>
+				<td>
+					<input type="text" name="description" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type="submit" value="submit" />
+				</td>
+			</tr>
+		</table>
+	</form>
+</div>
 </body>
 </html>
