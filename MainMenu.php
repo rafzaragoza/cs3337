b3ba88ae848b9c1dcbb0b9c74c06aa1d1cf48afd
mgillig <mgillig@calstatela.edu>
2019-05-08 22:28:39 -0700

    Update MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -1,5 +1,6 @@
 
 <link href="MainMenu.css" type="text/css" rel="stylesheet" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <?php
 	session_start();
@@ -7,24 +8,25 @@
 				'&email=' . $_SESSION['email'];
  #	print $name_email;
 ?>
-<h1> STUDENT BOOK EXCHANGE </h1>
-    <body>
+
 <ul class="ul_css">
-<li class="li_css">
-	<a class="li_css_a" href="shopping.php?<?php print $name_email; ?>" > shopping
-	</a>
-</li>
-<li class="li_css">
-	<a class="li_css_a" href="selling.php?<?php print $name_email; ?>" > selling
-	</a>
-</li>
-<li class="li_css">
-	<a class="li_css_a" href="profile.php?<?php print $name_email; ?>" > profile
-	</a>
-</li>
-<li class="li_css">
-	<a class="li_css_a" href="forum.php?<?php print $name_email; ?>" > forum
-	</a>
-</li>
+	<li class="li_css">
+		<h5 class="li_css_h">STUDENT BOOK EXCHANGE</h5>
+	</li>
+	<li class="li_css">
+		<a class="li_css_a" href="main.php?<?php print $name_email; ?>" > home
+		</a>
+	</li>
+	<li class="li_css">
+		<a class="li_css_a" href="shopping.php?<?php print $name_email; ?>" > shopping
+		</a>
+	</li>
+	<li class="li_css">
+		<a class="li_css_a" href="selling.php?<?php print $name_email; ?>" > selling
+		</a>
+	</li>
+	<li class="li_css">
+		<a class="li_css_a" href="forum.php?<?php print $name_email; ?>" > forum
+		</a>
+	</li>
 </ul>
-</body>
