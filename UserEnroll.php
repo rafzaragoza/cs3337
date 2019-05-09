499525030bdb280ad42a07942a1772c72a4ebaee
mgillig <mgillig@calstatela.edu>
2019-05-08 22:29:05 -0700

    Update UserEnroll.php

--- UserEnroll.php
+++ UserEnroll.php
@@ -2,35 +2,39 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="MainMenu.css" type="text/css" rel="stylesheet" />
 <title>User Enroll Page</title>
 </head>
 
 <body>
-<p align="center">User Enroll Page</p>
-<form action="UserInsert.php" method="post">
-<table align="center">
-<tr>
-<td>
-User Name
-</td>
-<td>
-<input type="text" name="name" />
-</td>
-</tr>
-<tr>
-<td>
-Email
-</td>
-<td>
-<input type="text" name="email" />
-</td>
-</tr>
-<tr>
-<td>
-<input type="submit" value="submit" />
-</td>
-</tr>
-</table>
-</form>
+<div class="container" style="background: white; width: 400px; margin-top: 10%">
+	<h1 class="div_css_h">User Enroll Page</h1>
+	<form action="UserInsert.php" method="post">
+		<table align="center">
+			<tr>
+				<td>
+					User Name
+				</td>
+				<td>
+					<input type="text" name="name" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Email
+				</td>
+				<td>
+					<input type="text" name="email" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input class="btn btn-dark" type="submit" value="submit" />
+				</td>
+			</tr>
+		</table>
+	</form>
+</div>
 </body>
 </html>
