46c50d8620cc87ca2b5e65a535fc985c5558e482
mgillig <mgillig@calstatela.edu>
2019-05-09 16:17:49 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,40 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Administrator Page</title>
+	<meta charset="utf-8">
+	<title>Administrator Page</title>
 </head>
 
 <body>
+	<?php
+		include("Connect_Database.php");
+	?>
 
-<?php
-	include("Connect_Database.php");
-?>
+	<?php
+		$searchUser = "select * from users where " .
+			"name='" . $_POST["name"] . "' and " .
+			"email='" . $_POST["email"] . "'";
+		print $_POST["name"];
+		print $searchUser;
+		$results = mysqli_query($connect, $searchUser);
+	?>
 
-<?php
-	$searchUser = "select * from users where " .
-		"name='" . $_POST["name"] . "' and " .
-		"email='" . $_POST["email"] . "'";
-	print $_POST["name"];
-	print $searchUser;
-	$results = mysqli_query($connect, $searchUser);
-?>
-
-<?php
-	if (mysqli_num_rows($results) == 0)
-	{
-		header("Location: login.html");
-		exit;
-	}
-	
-	if (mysqli_num_rows($results) > 0)
-	{
-		session_start();
-		$_SESSION['name'] = $_POST["name"];
-		$_SESSION['email'] = $_POST["email"];
-		header("Location: main.php");
-		exit;
-	}
-?>
+	<?php
+		if (mysqli_num_rows($results) == 0)
+		{
+			header("Location: login.html");
+			exit;
+		}
+		
+		if (mysqli_num_rows($results) > 0)
+		{
+			session_start();
+			$_SESSION['name'] = $_POST["name"];
+			$_SESSION['email'] = $_POST["email"];
+			header("Location: main.php");
+			exit;
+		}
+	?>
 </table>
 </body>
 </html>
